3e33e3de2ce2b72478f919e530415d7adda89390
Group Study <gasadullah1@gmail.com>
2022-08-22 23:00:17 -0500

    add g2

--- src/test/java/Com/nexttech/Java_groupstudy/Git2.java
+++ src/test/java/Com/nexttech/Java_groupstudy/Git2.java
@@ -1,10 +1,9 @@
 package Com.nexttech.Java_groupstudy;
 
-public class Git_Pull {
+public class Git2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi Idaya");
 
 	}
 
